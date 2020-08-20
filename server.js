e2f9262b2879a44c0f2a6fc4ba17126449f94b21
Troy Ian Verwayen <21884528@sun.ac.za>
2020-08-20 15:00:08 +0200

    Messing about a bit

--- server.js
+++ server.js
@@ -25,3 +25,19 @@ console.log('todo list RESTful API server started on: ' + port);
 app.get("/url", (req, res, next) => {
   res.json(["1","2","3","4","5"]);
  });
+
+app.get('/', (req, res) => {
+  return res.send('Received a GET HTTP method');
+});
+
+app.post('/', (req, res) => {
+  return res.send('Received a POST HTTP method');
+});
+
+app.put('/', (req, res) => {
+  return res.send('Received a PUT HTTP method');
+});
+
+app.delete('/', (req, res) => {
+  return res.send('Received a DELETE HTTP method');
+});
