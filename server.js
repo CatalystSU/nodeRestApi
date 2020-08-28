2b608946660f5732595251a4aaf9a6651718bf2e
Flippy_NB <justin.abrams@hotmail.com>
2020-08-28 09:54:05 +0200

    Next module chain

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ var neo4j = require('neo4j-driver');
 
 var driver = neo4j.driver(
   'bolt://robabrams.homeip.net:7687',
-  neo4j.auth.basic('neo4j', 'SuPr3m3L3d3r_TR0y')
+   neo4j.auth.basic('neo4j', 'SuPr3m3L3d3r_TR0y')
 )
 
 var session = driver.session();
--- test.js
+++ test.js
@@ -6,23 +6,23 @@ router.get("/url", (req, res, next) => {
     res.json(["1","2","3","4","5"]);
 });
   
-router.get('/', (req, res) => {
+router.get('/', (req, res, next) => {
     return res.send('Received a GET HTTP method');
 });
   
-router.post('/', (req, res) => {
+router.post('/', (req, res, next) => {
     return res.send('Received a POST HTTP method');
 });
   
-router.put('/', (req, res) => {
+router.put('/', (req, res, next) => {
     return res.send('Received a PUT HTTP method');
 });
   
-router.delete('/', (req, res) => {
+router.delete('/', (req, res, next) => {
     return res.send('Received a DELETE HTTP method');
 });
   
-router.post('/login', (req, res) => {
+router.post('/login', (req, res, next) => {
     res.status(200);
     return res.send('Logged in');
 });
