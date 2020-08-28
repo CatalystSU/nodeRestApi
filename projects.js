23bfdbbcb82a68bba7439b11157590dd9d7eda9c
Flippy_NB <justin.abrams@hotmail.com>
2020-08-28 10:13:59 +0200

    Create projct

--- projects.js
+++ projects.js
@@ -2,6 +2,24 @@ const express = require('express');
 const { create } = require('domain');
 const router = express.Router();
 
+var driver = require('./neo4j');
+
+router.post('/create', (req, res, next) => {
+    var session = driver.session();
+
+    session
+    .run('CREATE ($project_name:Project {id:$id})', req)
+    .then(function(result) {
+        result.records.forEach(function(record) {
+            console.log(record.get('title'))
+            console.log(record)
+        });
+        session.close();
+    })
+    .catch(function(error) {
+        console.log(error);
+    });
+})
 
 router.get('/get', (req, res, next) => {
     res.status(201).json({
