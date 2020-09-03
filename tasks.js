ac8ed7cc71c27659313e446f2b170ee6d1d85018
Troy Ian Verwayen <21884528@sun.ac.za>
2020-09-03 20:13:48 +0200

    postbruh works

--- tasks.js
+++ tasks.js
@@ -28,7 +28,7 @@ router.get('/all', (req, res, next) => {
     var session = driver.session();
     var projects = [];
     session
-    .run('MATCH (n:Project) RETURN n')
+    .run('MATCH (n:Task) RETURN n')
     .then(function(result) {
         result.records.forEach(function(record) {
             projects.push({name:record.get('n').properties.name, id:record.get('n').identity.low});
