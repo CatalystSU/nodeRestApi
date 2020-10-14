715f3d990558123a361ef0193ab3812a343a2b93
Flippy_NB <justin.abrams@hotmail.com>
2020-10-14 21:54:12 +0200

    Users

--- users.js
+++ users.js
@@ -12,17 +12,34 @@ const { SSL_OP_EPHEMERAL_RSA } = require('constants');
  */
 router.post('/create', (req, res, next) => {
     var session = driver.session();
+    var session1 = driver.session();
     var request = {
-        //TODO:
+        username:req.body.username,
+        email:req.body.email,
+        password:req.body.password
     }
+    console.log(request)
     session
-    .run('', request)
-    .then(function() {
-        res.status(200).json("Created User");
-        session.close;
+    .run('MATCH (u:User) WHERE u.email = $email RETURN u', request)
+    .then(function(result) {
+        if (result.records.length < 1) {
+            session1
+            .run('CREATE (u:User {username:$username, email:$email, password:$password})', request)
+            .then(function(result1) {
+                res.status(200).json("Created User");
+            })
+            .catch(function(error1) {
+                res.status(400).json("Couldn't create User");
+                session1.close
+                console.log(error1);
+            });
+        } else {
+            res.status(200).json("User already There");
+            session.close;
+        }
     })
     .catch(function(error) {
-        res.status(400).json("");//TODO:
+        res.status(400).json(error);//TODO:
         session.close
         console.log(error);
     });
