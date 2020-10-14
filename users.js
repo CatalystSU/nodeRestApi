db2d2e1f1658db2ffccd96d8980a9f1ccb5282e9
Troy Ian Verwayen <21884528@sun.ac.za>
2020-10-14 23:01:27 +0200

    auth

--- users.js
+++ users.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const { create } = require('domain');
 const router = express.Router();
-
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 var driver = require('./neo4j');
 const { waitForDebugger } = require('inspector');
 const { SSL_OP_EPHEMERAL_RSA } = require('constants');
@@ -23,18 +24,31 @@ router.post('/create', (req, res, next) => {
     .run('MATCH (u:User) WHERE u.email = $email RETURN u', request)
     .then(function(result) {
         if (result.records.length < 1) {
-            session1
-            .run('CREATE (u:User {username:$username, email:$email, password:$password})', request)
-            .then(function(result1) {
-                res.status(200).json("Created User");
-            })
-            .catch(function(error1) {
-                res.status(400).json("Couldn't create User");
-                session1.close
-                console.log(error1);
+            bcrypt.hash(req.body.password, 10, (err, hash) => {
+                request.password = hash
+                session1
+                .run('CREATE (u:User {username:$username, email:$email, password:$password})', request)
+                .then(function(result1) {
+                    const token = jwt.sign(
+                        {
+                            email: req.body.email,
+                            username: req.body.firstname
+                        },
+                        process.env.JWT_KEY, 
+                        {
+                            expiresIn: "2h"
+                        }
+                    );
+                    res.status(200).json({"message":"created user","token":token});
+                })
+                .catch(function(error1) {
+                    res.status(400).json("Couldn't create User");
+                    session1.close
+                    console.log(error1);
+                });
             });
         } else {
-            res.status(200).json("User already There");
+            res.status(400).json("User already There");
             session.close;
         }
     })
@@ -50,14 +64,46 @@ router.post('/create', (req, res, next) => {
  */
 router.post('/auth', (req, res, next) => {
     var session = driver.session();
+    u_password = ""
     var request = {
-        //TODO:
+        email:req.body.email,
+        password:req.body.password
     }
     session
-    .run('', request)
-    .then(function() {
-        res.status(200).json("Authed User");
-        session.close;
+    .run('MATCH (u:User) WHERE u.email = $email RETURN u', request)
+    .then(function(result) {
+        if (result.records.length == 1) {
+            var hashed = result.records[0].get("u").properties.password
+            var uname = result.records[0].get("u").properties.username
+            bcrypt.compare(req.body.password, hashed, (err, resul) => {
+                if (!resul) {
+                    res.status(401).json({message: "authentication Failed"});
+                    session.close;
+                } else if (resul) {
+                    const token = jwt.sign(
+                        {
+                            email: req.body.email,
+                            username: uname,
+                        },
+                        process.env.JWT_KEY,
+                        {
+                            expiresIn: "2h"
+                        }
+                    );
+                    return res.status(200).json({
+                        message: "authentication successful",
+                        token: token
+                    });
+                    session.close;
+                } else {
+                    return res.status(401).json({message: "authentication Failed"});
+                    session.close;
+                }
+            });
+        } else {
+            res.status(401).json({message: "authentication Failed"});
+            session.close;
+        }
     })
     .catch(function(error) {
         res.status(400).json("");//TODO:
