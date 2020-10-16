3ddb2cc0c5ab3b2311755022901ce38e4aed9089
Troy Ian Verwayen <21884528@sun.ac.za>
2020-10-16 11:00:29 +0200

    bruh

--- projects.js
+++ projects.js
@@ -74,7 +74,7 @@ router.post('/create',auth, (req, res, next) => {
 /**
  * Get All Project nodes
  */
-router.post('/all', auth, (req, res, next) => {
+router.get('/all', (req, res, next) => {
     var session = driver.session();
     var projects = [];
     session
@@ -92,81 +92,66 @@ router.post('/all', auth, (req, res, next) => {
     });
 });
 
-
 /**
  * Get Project Node via ID
  */
-//const auth = require('check-auth');
-router.post('/:id', auth, (req, res, next) => {
-    var session = driver.session();
-    var session1 = driver.session();
-    var session2 = driver.session();
-    var idd = req.params.id
-    var data = {
-        id: Number(req.params.id),
-        task_ob: {
-            tasks: [],
-            cons: []
-        }
-
-    };
-    var viewData = {};
-    var jsonData = {};
-    var request = {
-        id: Number(req.params.id)
-    }
-    Promise.all([
-        session.run('  MATCH (p:Project) WHERE ID(p) = $id \
-            RETURN p', request),
-
-        session1.run('  MATCH (p:Project) WHERE ID(p) = $id \
-            MATCH (p)<-[:UNDER]-(n) \
-            RETURN n', request),
-
-        session2.run('  MATCH (p:Project) WHERE ID(p) = $id \
-            MATCH (p)<-[:UNDER]-(n) \
-            MATCH (n)<-[:UNDER]-(n1) \
-            RETURN ID(n), ID(n1)', request)
-    ])
-    .then(function(results) {
-        // set the project name
-        data.name = results[0].records[0].get('p').properties.name
-
-        // get the tasks
-        var length = results[1].records.length;
-        let count = 0;
-        results[1].records.forEach(function(record) {
-            // I will assume even number of entries -> odd + odd = even and even + even = even
-            var temp
-            temp = record.get('n').properties
-            if (record.get('n').properties.taskprogress.low != null) {
-                temp.taskprogress = record.get('n').properties.taskprogress.low
+router.get('/temp/:id', (req, res, next) => {
+    if (req.params.id == 5) {
+        res.status(200).json({
+            name: "Epi_use5",
+            id: 5,
+            task_ob: {
+                tasks: [
+                    {taskname: "Task0", id:0, show: true, personincharge: "Freddie", packagemanager: "Bob", startdate: "15/10/2020", duration: 3, enddate: "01/11/2020", taskresources: "ben, fred", taskprogress: 50},
+                    {taskname: "Task1", id:1, show: true, personincharge: "Henry", packagemanager: "Bob", startdate: "16/10/2020", duration: 4, enddate: "12/11/2020", taskresources: "ben, jeff", taskprogress: 23},
+                    {taskname: "Task2", id:2, show: true, personincharge: "Chloe", packagemanager: "Bob", startdate: "25/10/2020", duration: 3, enddate: "15/12/2020", taskresources: "franny, fred", taskprogress: 70},
+                    {taskname: "Task3", id:3, show: true, personincharge: "Lucy", packagemanager: "Bob", startdate: "15/11/2020", duration: 5, enddate: "17/11/2020", taskresources: "ben, bob", taskprogress: 50},
+                    {taskname: "Task4", id:4, show: true, personincharge: "Nick", packagemanager: "Bob", startdate: "16/10/2020", duration: 3, enddate: "18/10/2020", taskresources: "joe, nic", taskprogress: 60},
+                ],
+                cons: [
+                    {from: 0, to: 1},
+                    {from: 0, to: 3},
+                    {from: 1, to: 2},
+                    {from: 2, to: 4},
+                    {from: 3, to: 4},
+                ]
             }
-            temp.task_id = record.get('n').identity.low
-            data.task_ob.tasks.push(temp);
-            count++;
         });
-
-        // get connections
-        results[2].records.forEach(function(record) {
-            temp = record._fields
-            data.task_ob.cons.push({"from":temp[1].low,"to":temp[0].low})
+    } else if (req.params.id == 6) {
+        res.status(200).json({
+            name: "Epi_use6",
+            id: 6,
+            task_ob: {
+                tasks: [
+                    {taskname: "Task0", id:0, show: true, personincharge: "Freddie", packagemanager: "Bob", startdate: "15/10/2020", duration: 3, enddate: "01/11/2020", taskresources: "ben, fred", taskprogress: 50},
+                    {taskname: "Task1", id:1, show: true, personincharge: "Henry", packagemanager: "Bob", startdate: "16/10/2020", duration: 4, enddate: "12/11/2020", taskresources: "ben, jeff", taskprogress: 23},
+                    {taskname: "Task2", id:2, show: true, personincharge: "Chloe", packagemanager: "Bob", startdate: "25/10/2020", duration: 3, enddate: "15/12/2020", taskresources: "franny, fred", taskprogress: 70},
+                    {taskname: "Task3", id:3, show: true, personincharge: "Lucy", packagemanager: "Bob", startdate: "15/11/2020", duration: 5, enddate: "17/11/2020", taskresources: "ben, bob", taskprogress: 50},
+                    {taskname: "Task4", id:4, show: true, personincharge: "Nick", packagemanager: "Bob", startdate: "16/10/2020", duration: 3, enddate: "18/10/2020", taskresources: "joe, nic", taskprogress: 60},
+                    {taskname: "Task5", id:5, show: true, personincharge: "Freddie", packagemanager: "Bob", startdate: "15/10/2020", duration: 3, enddate: "01/11/2020", taskresources: "ben, fred", taskprogress: 50},
+                    {taskname: "Task6", id:6, show: true, personincharge: "Henry", packagemanager: "Bob", startdate: "16/10/2020", duration: 4, enddate: "12/11/2020", taskresources: "ben, jeff", taskprogress: 23},
+                    {taskname: "Task7", id:7, show: true, personincharge: "Chloe", packagemanager: "Bob", startdate: "25/10/2020", duration: 3, enddate: "15/12/2020", taskresources: "franny, fred", taskprogress: 70},
+                ],
+                cons: [
+                    {from: 0, to: 1},
+                    {from: 0, to: 3},
+                    {from: 0, to: 7},
+                    {from: 1, to: 2},
+                    {from: 2, to: 4},
+                    {from: 3, to: 5},
+                    {from: 6, to: 1},
+                ]
+            }
         });
-
-        jsonData["results"] = results;
-        res.status(200).json(data);
-
-        session.close();
-        session1.close();
-        session2.close();
-    })
-    .catch(function(error) {
-        res.status(404).json({status:"id not found"})
-        console.log(error);
-    });
+    } else {
+        res.status(400).json({
+            message: "Not found"
+        });
+    };
 });
 
-router.get('/devvy/:id', auth, (req, res, next) => {
+
+router.get('/devvy/:id', (req, res, next) => {
     var idd = req.params.id
     var viewData = {};
     var jsonData = {};
@@ -259,7 +244,7 @@ function getProject(id) {
 
 
 
-router.post('/critical/:id', auth, (req, res, next) => {
+router.get('/critical/:id', (req, res, next) => {
     var session = driver.session();
     var session1 = driver.session();
     var session2 = driver.session();
@@ -403,7 +388,7 @@ function getDatum(str) {
 /**
  * Get all tasks with given resource
  */
-router.post('/resources', auth, (req, res, next) => {
+router.post('/resources', (req, res, next) => {
     var tasks = []
     var session = driver.session();
     var request = {
@@ -432,7 +417,7 @@ router.post('/resources', auth, (req, res, next) => {
 /**
  * Update Project node via ID
  */
-router.post('/update/:id', auth, (req, res, next) => {
+router.post('/update/:id', (req, res, next) => {
     var session = driver.session();
     var request = {
         id: Number(req.params.id),
@@ -470,5 +455,79 @@ router.delete('/:id', (req, res, next) => {
         console.log(error);
     });
 });
+
+/**
+ * Get Project Node via ID
+ */
+//const auth = require('check-auth');
+router.get('/:id', (req, res, next) => {
+    var session = driver.session();
+    var session1 = driver.session();
+    var session2 = driver.session();
+    var idd = req.params.id
+    var data = {
+        id: Number(req.params.id),
+        task_ob: {
+            tasks: [],
+            cons: []
+        }
+
+    };
+    var viewData = {};
+    var jsonData = {};
+    var request = {
+        id: Number(req.params.id)
+    }
+    Promise.all([
+        session.run('  MATCH (p:Project) WHERE ID(p) = $id \
+            RETURN p', request),
+
+        session1.run('  MATCH (p:Project) WHERE ID(p) = $id \
+            MATCH (p)<-[:UNDER]-(n) \
+            RETURN n', request),
+
+        session2.run('  MATCH (p:Project) WHERE ID(p) = $id \
+            MATCH (p)<-[:UNDER]-(n) \
+            MATCH (n)<-[:UNDER]-(n1) \
+            RETURN ID(n), ID(n1)', request)
+    ])
+    .then(function(results) {
+        // set the project name
+        data.name = results[0].records[0].get('p').properties.name
+
+        // get the tasks
+        var length = results[1].records.length;
+        let count = 0;
+        results[1].records.forEach(function(record) {
+            // I will assume even number of entries -> odd + odd = even and even + even = even
+            var temp
+            temp = record.get('n').properties
+            if (record.get('n').properties.taskprogress.low != null) {
+                temp.taskprogress = record.get('n').properties.taskprogress.low
+            }
+            temp.task_id = record.get('n').identity.low
+            data.task_ob.tasks.push(temp);
+            count++;
+        });
+
+        // get connections
+        results[2].records.forEach(function(record) {
+            temp = record._fields
+            data.task_ob.cons.push({"from":temp[1].low,"to":temp[0].low})
+        });
+
+        jsonData["results"] = results;
+        res.status(200).json(data);
+
+        session.close();
+        session1.close();
+        session2.close();
+    })
+    .catch(function(error) {
+        res.status(404).json({status:"id not found"})
+        console.log(error);
+    });
+});
+
 
 module.exports = router;
